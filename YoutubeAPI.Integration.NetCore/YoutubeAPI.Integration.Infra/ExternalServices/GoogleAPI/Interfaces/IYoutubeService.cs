da213e70711ba6845d31c2d4a6deaccdfb1e93be
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2019-09-29 20:16:53 -0300

    refactor: Implementation of External Services

---
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Infra/ExternalServices/GoogleAPI/Interfaces/IYoutubeService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeAPI.Integration.Infra.ExternalServices.GoogleAPI.Interfaces
+{
+    public interface IYoutubeService
+    {
+    }
+}
---
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Infra/ExternalServices/GoogleAPI/Services/YoutubeService.cs
@@ -0,0 +1,11 @@
+﻿using YoutubeAPI.Integration.Infra.ExternalServices.GoogleAPI.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YoutubeAPI.Integration.Infra.ExternalServices.GoogleAPI.Services
+{
+    public class YoutubeService : IYoutubeService
+    {
+    }
+}
