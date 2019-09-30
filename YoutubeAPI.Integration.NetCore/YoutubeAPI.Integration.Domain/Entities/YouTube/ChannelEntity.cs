e0b7421757299801f12af760dc403d06815076fb
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2019-09-29 23:29:46 -0300

    feat: Implementation of Entities

---
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Entities/YouTube/ChannelEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace YoutubeAPI.Integration.Domain.Entities.YouTube
+{
+    public class ChannelEntity
+    {
+        public string Name { get; set; }
+        public int Subscribers { get; set; }
+        public int Views { get; set; }
+        public int Videos { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            ChannelEntity compareObj = obj as ChannelEntity;
+            return this.Name.Equals(compareObj.Name);
+        } 
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+    }
+}
---
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Entities/YouTube/VideoEntity.cs
@@ -0,0 +1,26 @@
+﻿namespace YoutubeAPI.Integration.Domain.Entities.YouTube
+{
+    public class VideoEntity
+    {
+        public string Title { get; set; }
+        public string ChannelName { get; set; }
+        public string Duration { get; set; }
+        public int Likes { get; set; }
+        public int Dislikes { get; set; }
+        public int Comments { get; set; }
+        public int Views { get; set; }
+        public int Favorites { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            VideoEntity compareObj = obj as VideoEntity;
+            return Title.Equals(compareObj.Title)
+                && ChannelName.Equals(compareObj.ChannelName);
+        }
+
+        public override string ToString()
+        {
+            return this.Title.ToString();
+        }
+    }
+}
