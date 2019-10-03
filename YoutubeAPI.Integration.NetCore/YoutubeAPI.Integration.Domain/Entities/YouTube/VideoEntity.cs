50ba07add0cb835bb1a931d66d2b2fef22a2a011
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2019-10-03 15:48:22 -0300

    feat: Implementation of internal service manager

--- YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Entities/YouTube/VideoEntity.cs
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Entities/YouTube/VideoEntity.cs
@@ -1,19 +1,24 @@
-﻿namespace YoutubeAPI.Integration.Domain.Entities.YouTube
+﻿using System;
+using System.Collections.Generic;
+
+namespace YoutubeAPI.Integration.Domain.Entities.YouTube
 {
     public class VideoEntity
     {
         public string Title { get; set; }
         public string ChannelName { get; set; }
-        public string Duration { get; set; }
-        public int Likes { get; set; }
-        public int Dislikes { get; set; }
-        public int Comments { get; set; }
-        public int Views { get; set; }
-        public int Favorites { get; set; }
+        public TimeSpan Duration { get; set; }
+        public dynamic Thumbnails { get; set; }
+        public ulong? Likes { get; set; }
+        public ulong? Dislikes { get; set; }
+        public ulong? Comments { get; set; }
+        public ulong? Views { get; set; }
+        public ulong? Favorites { get; set; }
 
         public override bool Equals(object obj)
         {
-            VideoEntity compareObj = obj as VideoEntity;
+            if (!(obj is VideoEntity compareObj)) { return false; }
+
             return Title.Equals(compareObj.Title)
                 && ChannelName.Equals(compareObj.ChannelName);
         }
--- YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Interfaces/IYoutubeServiceManager.cs
+++ YoutubeAPI.Integration.NetCore/YoutubeAPI.Integration.Domain/Interfaces/IYoutubeServiceManager.cs
@@ -1,10 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using YoutubeAPI.Integration.Domain.Entities.YouTube;
+using YoutubeAPI.Integration.Domain.Enum;
 
 namespace YoutubeAPI.Integration.Domain.Interfaces
 {
     public interface IYoutubeServiceManager
     {
+        Task<List<VideoEntity>> GetPlaylistVideos(string oauthToken, PlaylistType playlist);
+        Task<ChannelEntity> GetChannel(string oauthToken);
     }
 }
