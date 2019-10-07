496f4213eb5cff788265c27e5e04dbf9dbf05e28
Emerson Alves <38816951+Katsshura@users.noreply.github.com>
2019-10-06 22:50:24 -0300

    feat: Implemented interation on html over array
    - Commented Code for testing purposes

--- YouTubeAPI-Integration-Angular/src/app/pages/home/home.component.ts
+++ YouTubeAPI-Integration-Angular/src/app/pages/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth.service';
 import {Router} from '@angular/router';
 import {YoutubeService} from '../../services/youtube.service';
 import {ChannelModel} from '../../models/channel.model';
 import {PlaylistType} from '../../enums/playlist-type.enum';
+import {VideoModel} from '../../models/video.model';
 
 @Component({
   selector: 'app-home',
@@ -13,6 +14,7 @@ import {PlaylistType} from '../../enums/playlist-type.enum';
 export class HomeComponent implements OnInit {
 
   private channel: ChannelModel;
+  private videos: VideoModel[] = [];
 
   constructor(private authService: AuthService, private router: Router, private youtubeService: YoutubeService) {
     // this.authService.UserSession.subscribe(auth => {
@@ -20,27 +22,54 @@ export class HomeComponent implements OnInit {
     //     this.router.navigate(['login']);
     //   }
     // });
+
   }
 
   ngOnInit() {
+    // this.channel = new ChannelModel();
+    // this.channel.name = 'Katsshura';
+    // this.channel.subscribers = 100;
+    // this.channel.videos = 100;
+    // this.channel.views = 800;
+    // this.channel.thumbnails = { medium: { url: 'https://i.pinimg.com/originals/f3/6d/4d/f36d4dbec4f480a4d8bc16cebf547ae9.jpg'}};
+    //
+    // console.log(this.channel);
+    // const video = new VideoModel();
+    // video.channelName = 'Katsshura\'s development';
+    // video.title = 'How to be a prostitute nowadays';
+    // video.thumbnails = 'https://i.pinimg.com/originals/f3/6d/4d/f36d4dbec4f480a4d8bc16cebf547ae9.jpg';
+    // video.duration = '12:00:00';
+    // video.comments = 90;
+    // video.likes = 10000;
+    // video.dislikes = 3000;
+    // video.favorites = 900;
+    // video.views = 1200000;
+    //
+    // this.videos.push(video);
+    // this.videos.push(video);
+    // this.videos.push(video);
     this.getChannel();
     // this.authService.singOut();
   }
 
   private logout() {
     this.authService.singOut(res => {
-      console.log(res);
       this.router.navigate(['login']);
     });
   }
 
   private async getChannel() {
-    this.channel = await this.youtubeService.getChannelInformation();
-    console.log(this.channel);
+    this.channel = await this.youtubeService.getChannelInformation(this.onGoogleTokenExpired.bind(this));
+    this.getPlaylist(PlaylistType.Upload);
   }
 
-  getPlaylist(playlistType: string) {
+  private async getPlaylist(playlistType: string | PlaylistType) {
     const playlist = PlaylistType[playlistType];
-    console.log(playlist);
+    this.videos = await this.youtubeService.getPlaylistVideos(playlist, this.onGoogleTokenExpired.bind(this));
+  }
+
+  private onGoogleTokenExpired(res: any) {
+    this.router.navigate(['login']);
+    // TODO: Implement toast message for expiration time
   }
 }
